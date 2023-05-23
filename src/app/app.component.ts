c8414a78c6bf304aaa1253f3a0f6eb32b3efc160
César Cornejo <csraco@hotmail.com>
2023-05-23 16:28:45 -0600

    6 String interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  // Can use private on variables here
+  // private name = 'Cesar';
+  name = 'Cesar';
+  age = 18;
+  img = 'https://cataas.com/cat';
 }
