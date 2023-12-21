56faa90304b67c827431892482b81ecf0d1fdf50
César Cornejo <csraco@hotmail.com>
2023-12-21 00:43:16 -0600

    Apis - enviar token en interceptor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -74,7 +74,7 @@ export class AppComponent {
   }
 
   getProfile() {
-    this.authService.profile(this.token)
+    this.authService.getProfile()
     .subscribe(profile => {
       console.log(profile);
     });
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,6 +4,9 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from "./../../environments/environment";
 import { Auth } from '../models/auth.module';
 import { User } from '../models/user.model';
+import { TokenService } from './token.service';
+
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,20 +16,20 @@ export class AuthService {
   private apiUrl = `${environment.API_URL}/api/auth`;
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private tokenService: TokenService,
   ) { }
 
   login(email: string, password: string) {
-    return this.http.post<Auth>(`${this.apiUrl}/login`, { email, password });
+    return this.http.post<Auth>(`${this.apiUrl}/login`, { email, password })
+    .pipe(
+      tap(response => this.tokenService.saveToken(response.access_token))
+    );
   }
 
-  profile(token: string) {
+  getProfile() {
     // let headers = new HttpHeaders();
     // headers = headers.set('Authorization', `Bearer ${token}`);
-    return this.http.get<User>(`${this.apiUrl}/profile`, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    });
+    return this.http.get<User>(`${this.apiUrl}/profile`);
   }
 }
